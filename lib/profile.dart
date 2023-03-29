1b83b3570db4f5bf39fae624fbf43ab4b8ba73f3
gillianneysha <125355806+gillianneysha@users.noreply.github.com>
2023-03-30 00:22:20 +0800

    edited box space, placed texts and other boxes
    Edited your opportunity box space, placed upcoming opportunity text and box and your organization text

--- lib/profile.dart
+++ lib/profile.dart
@@ -52,7 +52,7 @@ class Profile extends StatelessWidget {
           ),
 
           Container(
-            margin: EdgeInsets.fromLTRB(20.0, 20.0, 0.0, 0.0),
+            margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
             child: Row(
               children: [
                 PhysicalModel(
@@ -65,10 +65,38 @@ class Profile extends StatelessWidget {
             ),
           ),
 
+// --- Upcoming Opportunities text
           Container(
             margin: EdgeInsets.fromLTRB(20.0, 40, 0.0, 0.0),
             child: Text(
               'Upcoming Opportunities',
+              style: TextStyle(
+                fontSize: 20,
+                color: const Color(0xFF6E7181),
+                fontFamily: 'Questrial',
+              ),
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+            child: Row(
+              children: [
+                PhysicalModel(
+                  color: Colors.white,
+                  elevation: 5,
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(width: 350, height: 150),
+                ),
+              ],
+            ),
+          ),
+
+//  --- Your Organizations Text
+          Container(
+            margin: EdgeInsets.fromLTRB(20.0, 40, 0.0, 0.0),
+            child: Text(
+              'Your Organizations',
               style: TextStyle(
                 fontSize: 20,
                 color: const Color(0xFF6E7181),
