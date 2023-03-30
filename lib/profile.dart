7a9a7aae91966eec7360adf4fa7872a7f4d004ef
gillianneysha <125355806+gillianneysha@users.noreply.github.com>
2023-03-31 00:26:46 +0800

    changed color

--- lib/profile.dart
+++ lib/profile.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
   String name = "";
-  int hours = 0;
+  int hours = 14;
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
 
   @override
   Widget build(BuildContext context) {
@@ -12,112 +14,170 @@ class Profile extends StatelessWidget {
         elevation: 10,
         backgroundColor: const Color(0xFF89C0CC),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(20.0, 30.0, 0.0, 0.0),
-            child: Text(
-              'Welcome, $name !',
-              style: TextStyle(
-                fontSize: 20,
-                color: const Color(0xFF6E7191),
-                fontFamily: 'Questrial',
-              ),
-            ),
-          ),
-// --- Hours text
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
-            child: Text(
-              '$hours',
-              style: TextStyle(
-                fontSize: 30,
-                color: const Color(0xFF6E7191),
-                fontFamily: 'Questrial',
-              ),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.fromLTRB(20.0, 40, 0.0, 0.0),
-            child: Text(
-              'Your Opportunity',
-              style: TextStyle(
-                fontSize: 20,
-                color: const Color(0xFF6E7181),
-                fontFamily: 'Questrial',
-              ),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
-            child: Row(
-              children: [
-                PhysicalModel(
-                  color: Colors.white,
-                  elevation: 5,
-                  borderRadius: BorderRadius.circular(15),
-                  child: Container(width: 350, height: 150),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 30),
+                width: 550,
+                height: 30,
+                child: Material(
+                  color: const Color(0xFF89C0CC),
+                  child: Text(
+                    'Welcome, Ysha!',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: 'Questrial',
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          ),
+              ),
+              // Container(
+              //   margin: EdgeInsets.fromLTRB(20.0, 30.0, 0.0, 0.0),
+              //   child: Text(
+              //     'Welcome, $name !',
+              //     style: TextStyle(
+              //       fontSize: 30,
+
+              //       fontFamily: 'Questrial',
+              //     ),
+              //   ),
+              // ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+                child: Text(
+                  'Hours Volunteered',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: const Color(0xFF6E7191),
+                    fontFamily: 'Questrial',
+                  ),
+                ),
+              ),
+// --- Hours text
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+                child: Text(
+                  '$hours h',
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: const Color(0xFF6E7191),
+                    fontFamily: 'Questrial',
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 40, 0.0, 0.0),
+                child: Text(
+                  'Your Opportunity',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: const Color(0xFF6E7181),
+                    fontFamily: 'Questrial',
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+                child: Row(
+                  children: [
+                    PhysicalModel(
+                      color: Colors.white,
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(width: 350, height: 150),
+                    ),
+                  ],
+                ),
+              ),
 
 // --- Upcoming Opportunities text
-          Container(
-            margin: EdgeInsets.fromLTRB(20.0, 40, 0.0, 0.0),
-            child: Text(
-              'Upcoming Opportunities',
-              style: TextStyle(
-                fontSize: 20,
-                color: const Color(0xFF6E7181),
-                fontFamily: 'Questrial',
-              ),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
-            child: Row(
-              children: [
-                PhysicalModel(
-                  color: Colors.white,
-                  elevation: 5,
-                  borderRadius: BorderRadius.circular(15),
-                  child: Container(width: 350, height: 150),
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 40, 0.0, 0.0),
+                child: Text(
+                  'Upcoming Opportunities',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: const Color(0xFF6E7181),
+                    fontFamily: 'Questrial',
+                  ),
                 ),
-              ],
-            ),
-          ),
+              ),
+
+//               Container(
+//                 margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+//                 child: Row(
+//                   children: [
+//                     PhysicalModel(
+//                       color: Colors.white,
+//                       elevation: 5,
+//                       borderRadius: BorderRadius.circular(15),
+//                       child: Container(width: 350, height: 150),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+                child: Row(
+                  children: [
+                    PhysicalModel(
+                      color: Colors.white,
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
+                        width: 350,
+                        height: 150,
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          'Date: Sorsogon City',
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: const Color(0xFF6E7181),
+                            fontFamily: 'Questrial',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 
 //  --- Your Organizations Text
-          Container(
-            margin: EdgeInsets.fromLTRB(20.0, 40, 0.0, 0.0),
-            child: Text(
-              'Your Organizations',
-              style: TextStyle(
-                fontSize: 20,
-                color: const Color(0xFF6E7181),
-                fontFamily: 'Questrial',
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 40, 0.0, 0.0),
+                child: Text(
+                  'Your Organizations',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: const Color(0xFF6E7181),
+                    fontFamily: 'Questrial',
+                  ),
+                ),
               ),
-            ),
-          ),
 
-          Container(
-            margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
-            child: Row(
-              children: [
-                PhysicalModel(
-                    color: Colors.white,
-                    elevation: 5,
-                    child: Container(width: 90, height: 90),
-                    shape: BoxShape.circle),
-              ],
-            ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+                child: Row(
+                  children: [
+                    PhysicalModel(
+                        color: Colors.white,
+                        elevation: 5,
+                        child: Container(width: 90, height: 90),
+                        shape: BoxShape.circle),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
