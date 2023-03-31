e8c6437bcd163286c48e7bf36ffc64696660d25e
gillianneysha <125355806+gillianneysha@users.noreply.github.com>
2023-04-01 01:46:11 +0800

    added UpOpp redirect

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:solutions_challenge_app/VolunteeringOpportunities/opportunitiesPage.dart';
 import 'package:solutions_challenge_app/VolunteeringOpportunities/organizationPage.dart';
 
 class profile extends StatefulWidget {
@@ -134,145 +135,159 @@ class _profileState extends State<profile> {
                 margin: EdgeInsets.only(top: 6, left: 20),
                 width: 350,
                 height: 150,
-                child: Material(
-                  elevation: 3,
-                  color: Colors.white,
-                  shadowColor: Colors.black,
-                  borderRadius: BorderRadius.circular(20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // --- Date text
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 20.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Date:',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => OpportunitiesPage()),
+                    );
+                  },
+                  child: Material(
+                    elevation: 3,
+                    color: Colors.white,
+                    shadowColor: Colors.black,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // --- Date text
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 20.0, right: 26.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Date:',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 13,
+                                      fontFamily: 'Questrial',
+                                      color: const Color(0xFF89C0CC),
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  ' 11PM - 5AM October 25, 2022',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
+                                  Text(
+                                    ' 11PM - 5AM October 25, 2022',
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      fontFamily: 'Questrial',
+                                      color: const Color(0xFF89C0CC),
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
 
-                          // --- Location text
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 5.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Location',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
+                            // --- Location text
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 5.0, right: 26.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Location',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 13,
+                                      fontFamily: 'Questrial',
+                                      color: const Color(0xFF89C0CC),
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  ' Sorsogon City',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
+                                  Text(
+                                    ' Sorsogon City',
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      fontFamily: 'Questrial',
+                                      color: const Color(0xFF89C0CC),
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
 
-                          // --- name of Org
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 15.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Red Cross Sorsogon Chapter',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
+                            // --- name of Org
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 15.0, right: 26.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Red Cross Sorsogon Chapter',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 13,
+                                      fontFamily: 'Questrial',
+                                      color: const Color(0xFF89C0CC),
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
 
-                          // --- Service
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 5.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Emergency Services',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
+                            // --- Service
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 5.0, right: 26.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Emergency Services',
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      fontFamily: 'Questrial',
+                                      color: const Color(0xFF89C0CC),
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
 
-                          // --- Difficulty text
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 5.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Difficulty: ',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
+                            // --- Difficulty text
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 5.0, right: 26.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Difficulty: ',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 13,
+                                      fontFamily: 'Questrial',
+                                      color: const Color(0xFF89C0CC),
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  ' High',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: Colors.red,
+                                  Text(
+                                    ' High',
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      fontFamily: 'Questrial',
+                                      color: Colors.red,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
 
 // --- LOGO
-                        ], // stop here
-                      ),
-                    ],
+                          ], // stop here
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -307,7 +322,7 @@ class _profileState extends State<profile> {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => OrganizationPage()),
+                                    builder: (context) => OpportunitiesPage()),
                               );
                             }, // should not be moved
                             child: Container(
