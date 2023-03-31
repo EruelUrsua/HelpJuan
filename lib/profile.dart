0a5cf5d6ea5893f6113f5532821d53004fe969c8
gillianneysha <125355806+gillianneysha@users.noreply.github.com>
2023-04-01 01:27:32 +0800

    added horizontal scroll to UpOpp box

--- lib/profile.dart
+++ lib/profile.dart
@@ -9,7 +9,7 @@ class profile extends StatefulWidget {
 }
 
 class _profileState extends State<profile> {
-  final List<String> items = List.generate(100, (index) => "Item $index");
+  final List<String> items = List.generate(10, (index) => "Item $index");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -292,155 +292,192 @@ class _profileState extends State<profile> {
 
 // Upcoming opp box
               Container(
-                margin: EdgeInsets.only(top: 6, left: 20),
-                width: 350,
-                height: 150,
-                child: Material(
-                  elevation: 3,
-                  color: Colors.white,
-                  shadowColor: Colors.black,
-                  borderRadius: BorderRadius.circular(20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // --- Date text
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 20.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Date:',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
-                                  ),
+                height: 180.0,
+                child: ListView.builder(
+                  padding: EdgeInsets.only(left: 20.0),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => OrganizationPage()),
+                              );
+                            }, // should not be moved
+                            child: Container(
+                              margin: EdgeInsets.only(top: 6, left: 20),
+                              width: 350,
+                              height: 150,
+                              child: Material(
+                                elevation: 3,
+                                color: Colors.white,
+                                shadowColor: Colors.black,
+                                borderRadius: BorderRadius.circular(20),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+// --- Date text
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 20.0, right: 26.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                'Date:',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 13,
+                                                  fontFamily: 'Questrial',
+                                                  color:
+                                                      const Color(0xFF89C0CC),
+                                                ),
+                                              ),
+                                              Text(
+                                                ' 11PM - 5AM October 25, 2022',
+                                                style: TextStyle(
+                                                  fontSize: 13,
+                                                  fontFamily: 'Questrial',
+                                                  color:
+                                                      const Color(0xFF89C0CC),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        // --- Location text
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 5.0, right: 26.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                'Location',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 13,
+                                                  fontFamily: 'Questrial',
+                                                  color:
+                                                      const Color(0xFF89C0CC),
+                                                ),
+                                              ),
+                                              Text(
+                                                ' Sorsogon City',
+                                                style: TextStyle(
+                                                  fontSize: 13,
+                                                  fontFamily: 'Questrial',
+                                                  color:
+                                                      const Color(0xFF89C0CC),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+
+                                        // --- name of Org
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 15.0, right: 26.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                'Red Cross Sorsogon Chapter',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 13,
+                                                  fontFamily: 'Questrial',
+                                                  color:
+                                                      const Color(0xFF89C0CC),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+
+                                        // --- Service
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 5.0, right: 26.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                'Emergency Services',
+                                                style: TextStyle(
+                                                  fontSize: 13,
+                                                  fontFamily: 'Questrial',
+                                                  color:
+                                                      const Color(0xFF89C0CC),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+
+                                        // --- Difficulty text
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 5.0, right: 26.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                'Difficulty: ',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 13,
+                                                  fontFamily: 'Questrial',
+                                                  color:
+                                                      const Color(0xFF89C0CC),
+                                                ),
+                                              ),
+                                              Text(
+                                                ' High',
+                                                style: TextStyle(
+                                                  fontSize: 13,
+                                                  fontFamily: 'Questrial',
+                                                  color: Colors.red,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
-                                Text(
-                                  ' 11PM - 5AM October 25, 2022',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
-                                  ),
-                                ),
-                              ],
+                              ),
                             ),
                           ),
-
-                          // --- Location text
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 5.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Location',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
-                                  ),
-                                ),
-                                Text(
-                                  ' Sorsogon City',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-
-                          // --- name of Org
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 15.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Red Cross Sorsogon Chapter',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-
-                          // --- Service
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 5.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Emergency Services',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-
-                          // --- Difficulty text
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 5.0, right: 26.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Difficulty: ',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: const Color(0xFF89C0CC),
-                                  ),
-                                ),
-                                Text(
-                                  ' High',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'Questrial',
-                                    color: Colors.red,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-
-// --- LOGO
-                        ], // stop here
+                        ], // should not be moved
                       ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
               ),
 
 // --- Your organizations text
               Container(
-                margin: EdgeInsets.fromLTRB(20.0, 20, 0.0, 0.0),
+                margin: EdgeInsets.fromLTRB(20.0, 5, 0.0, 0.0),
                 child: Text(
                   'Your Organizations',
                   style: TextStyle(
