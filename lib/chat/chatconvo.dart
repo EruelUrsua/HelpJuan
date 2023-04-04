e08f3f8b34e003961f08ea826931b03b645db70f
vncmatthew <mattkleong@gmail.com>
2023-04-04 18:23:19 +0800

    edited chatConvo.dart

--- lib/chat/chatconvo.dart
+++ lib/chat/chatconvo.dart
@@ -10,30 +10,76 @@ class chatConvoPage extends StatefulWidget {
 class _chatConvoPageState extends State<chatConvoPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // SingleChildScrollView(),
-        Container(
-          alignment: Alignment.bottomCenter,
-          margin: EdgeInsetsDirectional.only(top: 20),
-          child: TextField(
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                  borderSide: BorderSide.none,
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Container(
+              child: Material(
+                  child: CircleAvatar(
+                    radius: 20.0,
+                    backgroundColor: Colors.white,
+                    child: Image.asset('assets/images/RedAlert.png',
+                        fit: BoxFit.contain),
+                  ),
+                  shape: const CircleBorder(side: BorderSide.none)),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  child: Text(
+                    'Red Alert Local Chapter',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13.0,
+                    ),
+                  ),
                 ),
-                hintText: "Write a message...",
-                hintStyle: TextStyle(
-                  fontSize: 16,
-                  color: Color(0xFF58A7B9),
+                Container(
+                  margin: EdgeInsets.only(left: 10, top: 2),
+                  child: Text(
+                    '7 online, form 12 people',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10.0,
+                    ),
+                  ),
                 ),
-                prefixIcon: Icon(Icons.send),
-                prefixIconColor: Color(0xFF58A7B9)),
-          ),
+              ],
+            )
+          ],
         ),
-      ],
+      ),
+      body: Column(
+        children: [
+          // SingleChildScrollView(),
+          Expanded(
+              child: Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: Container(
+              margin: EdgeInsetsDirectional.only(top: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintText: "Write a message...",
+                    hintStyle: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF58A7B9),
+                    ),
+                    prefixIcon: Icon(Icons.emoji_emotions),
+                    prefixIconColor: Color(0xFF58A7B9)),
+              ),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
